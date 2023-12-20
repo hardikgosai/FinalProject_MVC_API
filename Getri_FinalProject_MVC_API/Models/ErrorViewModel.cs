dd0171b533aacb06fc6c50158659bdb6c4ea51f6
Hardikgiri Gosai <hardik.gosai007@gmail.com>
2023-12-20 12:07:35 +0530

    Add project files.

---
+++ Getri_FinalProject_MVC_API/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Getri_FinalProject_MVC_API.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
