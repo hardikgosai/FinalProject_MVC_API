b34a9f46cc676a6d5bf39d96545d368b8287b612
Hardikgiri Gosai <hardik.gosai007@gmail.com>
2023-12-20 16:53:19 +0530

    CRUD completed

--- Getri_FinalProject_MVC_API/Controllers/ProductCategoryController.cs
+++ Getri_FinalProject_MVC_API/Controllers/ProductCategoryController.cs
@@ -8,13 +8,11 @@ namespace Getri_FinalProject_MVC_API.Controllers
     public class ProductCategoryController : Controller
     {
         HttpClient _client;
-        IConfiguration configuration;
-        private readonly ICategoryRepository categoryRepository;
+        IConfiguration configuration;        
 
-        public ProductCategoryController(IConfiguration _configuration, ICategoryRepository _categoryRepository)
+        public ProductCategoryController(IConfiguration _configuration)
         {
             configuration = _configuration;
-            categoryRepository = _categoryRepository;
             string apiAddress = configuration["ApiAddress"];
             Uri baseAddress = new Uri(apiAddress);
             _client = new HttpClient { BaseAddress = baseAddress };
@@ -29,6 +27,107 @@ namespace Getri_FinalProject_MVC_API.Controllers
                 var result = response.Content.ReadAsStringAsync().Result;
                 model = JsonConvert.DeserializeObject<List<CategoryWithIdViewModel>>(result);
             }
+            return View(model);
+        }
+
+        public async Task<IActionResult> CategoryDetails(int id)
+        {
+			CategoryWithIdViewModel model = new CategoryWithIdViewModel();
+			HttpResponseMessage response = await _client.GetAsync("api/Category/GetCategoryById?id=" + id);
+			if (response.IsSuccessStatusCode)
+            {
+				var result = response.Content.ReadAsStringAsync().Result;
+				model = JsonConvert.DeserializeObject<CategoryWithIdViewModel>(result);
+			}
+			return View(model);
+		}
+
+        [HttpGet]
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateCategory(CategoryViewModel model)
+        {
+            if (ModelState.IsValid)
+            {             
+                HttpResponseMessage response = await _client.PostAsJsonAsync("api/Category/InsertCategory", model);
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Error while creating record");
+                }
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditCategory(int id)
+        {
+            CategoryWithIdViewModel model = new CategoryWithIdViewModel();
+            HttpResponseMessage response = await _client.GetAsync("api/Category/GetCategoryById?id=" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                var result = response.Content.ReadAsStringAsync().Result;
+                model = JsonConvert.DeserializeObject<CategoryWithIdViewModel>(result);
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditCategory(CategoryWithIdViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                HttpResponseMessage response = await _client.PutAsJsonAsync("api/Category/EditCategory", model);
+                if (response.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Error while updating record");
+                }
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+            CategoryWithIdViewModel model = new CategoryWithIdViewModel();
+            HttpResponseMessage response = await _client.GetAsync("api/Category/GetCategoryById?id=" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                var result = response.Content.ReadAsStringAsync().Result;
+                model = JsonConvert.DeserializeObject<CategoryWithIdViewModel>(result);
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteCategory(CategoryWithIdViewModel model)
+        {
+            HttpResponseMessage response = await _client.DeleteAsync("api/Category/RemoveCategory?id=" + model.CategoryId);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Error while deleting record");
+            }
+
             return View(model);
         }
     }
---
+++ Getri_FinalProject_MVC_API/ViewModel/CategoryViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Getri_FinalProject_MVC_API.ViewModel
+{
+    public class CategoryViewModel
+    {
+        public string CategoryName { get; set; }
+    }
+}
